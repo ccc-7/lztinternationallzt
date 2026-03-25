04edcaa1b58db5cce553e3979f1be8f272303a10
ccc-7 <2457319307@qq.com>
2026-03-25 10:32:42 +0800

    更新代码框架以及assets文件

--- src/main/java/edu/bupt/ta/model/Job.java
+++
@@ -1,8 +0,0 @@
-package edu.bupt.ta.model;
-
-/**
- * Reserved: TA job posting entity.
- * Fields: jobId, title, moduleCode, organiser, minYear, maxYear, hours, status, etc.
- */
-public class Job {
-}
